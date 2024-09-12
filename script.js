88f968fd6825b8190373b9c5152e7370a6accee9
kevin <kevinrahul.22ec@kct.ac.in>
2024-09-12 20:44:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website is ready!");
+    
+    const recommendations = [
+      { type: "Beach", images: ["beach1.jpg", "beach2.jpg"] },
+      { type: "Temple", images: ["temple1.jpg", "temple2.jpg"] },
+      { type: "Country", images: ["japan1.jpg", "japan2.jpg"] }
+    ];
+  });
+  
